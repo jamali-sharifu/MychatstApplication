ba82f40b0f9990fcd94c4c526d281fa3333cd696
jamali-sharifu <84394828+jamali-sharifu@users.noreply.github.com>
2022-05-23 13:18:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mychatstapplication/setprofile.java
@@ -0,0 +1,14 @@
+package com.example.mychatstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setprofile);
+    }
+}
---
+++ app/src/main/java/com/example/mychatstapplication/splashScreen.java
@@ -0,0 +1,20 @@
+package com.example.mychatstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splashScreen extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
+
